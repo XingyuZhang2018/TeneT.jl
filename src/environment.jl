189325ff0ef434b7451177e1f081c02d3252b5f5
XingyuZhang2018 <xinzaijiuhao@gmail.com>
2025-06-04 22:30:53 +0200

    forloop for leg5

--- src/environment.jl
+++ src/environment.jl
@@ -308,6 +308,7 @@ FLᵢⱼ₊₁ =   FLᵢⱼ ─ Mᵢⱼ   ──                     ├─ d �
 
 FLmap(FL, ALu, ALd, M::leg4) = ein"((adf,fgh),dgeb),abc -> ceh"(FL, ALd, M, ALu)
 FLmap(FL, ALu, ALd, M::leg5) = ein"(((aefi,ijkl),ejgbp),fkhcp),abcd -> dghl"(FL, ALd, M, conj(M), ALu)
+FLmap(FL, ALu, ALd, M1::leg5, M2::leg5) = ein"(((aefi,ijkl),ejgbp),fkhcp),abcd -> dghl"(FL, ALd, M1, M2, ALu)
 FLmap(FL, ALu, ALd, M::leg8) = ein"((aefi,ijkl),efjkghbc),abcd -> dghl"(FL, ALd, M, ALu)
 
 function FLmap_forloop(FL, ALu, ALd, M; forloop_iter=1)
@@ -327,6 +328,23 @@ function FLmap_forloop(FL, ALu, ALd, M; forloop_iter=1)
         cols = fill(:,ndims(FL)-1)
         for i in χ_ranges
             FLm[cols...,i] = checkpoint(FLmap, FL, ALu, ALd[cols...,i], M)
+        end
+        return copy(FLm)
+    end
+end
+
+function FLmap_forloop(FL, ALu, ALd, M1, M2; forloop_iter=1)
+    if forloop_iter == 1
+        return FLmap(FL, ALu, ALd, M1, M2)
+    else
+        χ = size(FL, 1)
+        D = size(M1, 5)
+        FLm = Zygote.Buffer(FL, χ,D,D,χ)
+        χ_loop = cld(χ, forloop_iter)
+        χ_ranges = [range(1 + (i-1)*χ_loop, min(i*χ_loop, χ)) for i in 1:forloop_iter]
+        cols = fill(:,ndims(FL)-1)
+        for i in χ_ranges
+            FLm[cols...,i] = checkpoint(FLmap, FL, ALu, ALd[cols...,i], M1, M2)
         end
         return copy(FLm)
     end
@@ -354,6 +372,7 @@ end
 """
 FRmap(FR, ARu, ARd, M::leg4) = ein"((abc,ceh),dgeb),fgh -> adf"(ARu, FR, M, ARd)
 FRmap(FR, ARu, ARd, M::leg5) = ein"(((abcd,dghl),ejgbp),fkhcp),ijkl -> aefi"(ARu, FR, M, conj(M), ARd)
+FRmap(FR, ARu, ARd, M1::leg5, M2::leg5) = ein"(((abcd,dghl),ejgbp),fkhcp),ijkl -> aefi"(ARu, FR, M1, M2, ARd)
 FRmap(FR, ARu, ARd, M::leg8) = ein"((abcd,dghl),efjkghbc),ijkl -> aefi"(ARu, FR, M, ARd)
 
 function FRmap_forloop(FR, ARu, ARd, M; forloop_iter=1) 
@@ -373,6 +392,23 @@ function FRmap_forloop(FR, ARu, ARd, M; forloop_iter=1)
         cols = fill(:,ndims(FR)-1)
         for i in χ_ranges
             FRm[i,cols...] = checkpoint(FRmap, FR, ARu[i,cols...], ARd, M)
+        end
+        return copy(FRm)
+    end
+end
+
+function FRmap_forloop(FR, ARu, ARd, M1, M2; forloop_iter=1) 
+    if forloop_iter == 1
+        return FRmap(FR, ARu, ARd, M1, M2)
+    else
+        χ = size(FR, 1)
+        D = size(M1, 1)
+        FRm = Zygote.Buffer(FR, χ,D,D,χ)
+        χ_loop = cld(χ, forloop_iter)
+        χ_ranges = [range(1 + (i-1)*χ_loop, min(i*χ_loop, χ)) for i in 1:forloop_iter]
+        cols = fill(:,ndims(FR)-1)
+        for i in χ_ranges
+            FRm[i,cols...] = checkpoint(FRmap, FR, ARu[i,cols...], ARd, M1, M2)
         end
         return copy(FRm)
     end
@@ -597,6 +633,7 @@ end
 """
 ACmap(AC, FL, FR, M::leg4) = ein"((abc,ceh),dgeb),adf -> fgh"(AC,FR,M,FL)
 ACmap(AC, FL, FR, M::leg5) = ein"(((abcd,dghl),ejgbp),fkhcp),aefi -> ijkl"(AC,FR,M,conj(M),FL)
+ACmap(AC, FL, FR, M1::leg5, M2::leg5) = ein"(((abcd,dghl),ejgbp),fkhcp),aefi -> ijkl"(AC,FR,M1,M2,FL)
 ACmap(AC, FL, FR, M::leg8) = ein"((abcd,dghl),efjkghbc),aefi -> ijkl"(AC,FR,M,FL)
 
 function ACmap_forloop(AC, FL, FR, M; forloop_iter=1) 
@@ -616,6 +653,23 @@ function ACmap_forloop(AC, FL, FR, M; forloop_iter=1)
         cols = fill(:,ndims(AC)-1)
         for i in χ_ranges
             ACm[cols...,i] = checkpoint(ACmap, AC,FL,FR[cols...,i],M)
+        end
+        return copy(ACm)
+    end
+end
+
+function ACmap_forloop(AC, FL, FR, M1, M2; forloop_iter=1) 
+    if forloop_iter == 1
+        return ACmap(AC, FL, FR, M1, M2)
+    else
+        χ = size(AC)[end]
+        D = size(M1, 3)
+        ACm = Zygote.Buffer(AC, χ,D,D,χ)
+        χ_loop = cld(χ, forloop_iter)
+        χ_ranges = [range(1 + (i-1)*χ_loop, min(i*χ_loop, χ)) for i in 1:forloop_iter]
+        cols = fill(:,ndims(AC)-1)
+        for i in χ_ranges
+            ACm[cols...,i] = checkpoint(ACmap, AC,FL,FR[cols...,i],M1,M2)
         end
         return copy(ACm)
     end
