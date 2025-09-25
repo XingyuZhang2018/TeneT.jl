28fb248297190dfe639d016c198feee7fa383316
XingyuZhang2018 <xinzaijiuhao@gmail.com>
2025-09-25 18:26:27 +0200

    readd forloop_sum

--- src/contraction/forloop.jl
+++ src/contraction/forloop.jl
@@ -90,6 +90,27 @@ function ACdmap_forloop(ACd, FL, FR, M; forloop_iter)
     return forloop(ACdmap, ACd, FL, FR, M; forloop_iter, N_in, N_out, size_out)
 end
 
+function forloop_sum(f, args...; forloop_iter, N_in1, N_in2, size_out)
+    if forloop_iter == 1
+        return f(args...)
+    else
+        D_split = size(args[N_in1[1]])[N_in1[2]]
+        result = similar(args[1], size_out)
+        result .= 0
+        D_split_loop = cld(D_split, forloop_iter)
+        D_split_ranges = [range(1 + (i-1)*D_split_loop, min(i*D_split_loop, D_split)) for i in 1:forloop_iter]
+
+        for range in D_split_ranges
+            cols_in1 = (j == N_in1[2] ? range : (:) for j in 1:ndims(args[N_in1[1]]))
+            cols_in2 = (j == N_in2[2] ? range : (:) for j in 1:ndims(args[N_in2[1]]))
+            split_args = (j == N_in1[1] ? args[j][cols_in1...] : (j == N_in2[1] ? args[j][cols_in2...] : args[j]) for j in 1:length(args))
+            result += f(split_args...)
+        end
+        
+        return result
+    end
+end
+
 function Mumap_forloop(AC, ACd, FL, FR, Mu; forloop_iter)
     N_in1 = (2, 4)
     N_in2 = (4, 4)
