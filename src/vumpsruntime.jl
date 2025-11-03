91c391af3a3b805931655bfbd7794081dc59bd29
XingyuZhang2018 <xinzaijiuhao@gmail.com>
2025-11-03 16:37:22 +0100

    without conj for Ad

--- src/vumpsruntime.jl
+++ src/vumpsruntime.jl
@@ -28,9 +28,9 @@ function init_VUMPSRuntime(M, χ::Int, alg::VUMPS)
     return VUMPSRuntime(AL, AR, C, FL, FR)
 end
 
-_down_m(m::leg4) = permutedims(conj(m), (1,4,3,2))
-_down_m(m::leg5) = permutedims(conj(m), (1,4,3,2,5))
-_down_m(m::leg8) = permutedims(conj(m), (1,2,7,8,5,6,3,4))
+_down_m(m::leg4) = permutedims(m, (1,4,3,2))
+_down_m(m::leg5) = permutedims(m, (1,4,3,2,5))
+_down_m(m::leg8) = permutedims(m, (1,2,7,8,5,6,3,4))
 function _down_M(M::StructArray)
     Ni, Nj = size(M)
     pattern_d = copy(M.pattern)
@@ -154,10 +154,10 @@ function VUMPSEnv(rt::VUMPSRuntime, M::StructArray, alg::VUMPS, Fo=[rt.FL, rt.FR
     # ACd = StructArray([perm(AC.data[1])], AC.pattern)
     # ALd = StructArray([perm(AR.data[1])], AL.pattern)
     # ARd = StructArray([perm(AL.data[1])], AR.pattern)
-    # _, FLo =  leftenv(AL, ALd, M, Fo[1]; ifobs = true, alg)
-    # _, FRo = rightenv(AR, ARd, M, Fo[2]; ifobs = true, alg)
+    _, FLo =  leftenv(AL, AL, M, Fo[1]; ifobs = true, alg)
+    _, FRo = rightenv(AR, AR, M, Fo[2]; ifobs = true, alg)
     # return VUMPSEnv(AC, AR, ACd, ARd, FL, FR, FLo, FRo)
-    return VUMPSEnv(AC, AR, AC, AR, FL, FR, FL, FR)
+    return VUMPSEnv(AC, AR, AC, AR, FL, FR, FLo, FRo)
 end
 
 function leading_boundary(rt::Tuple{VUMPSRuntime, VUMPSRuntime}, M::StructArray, alg::VUMPS)
@@ -198,8 +198,8 @@ function VUMPSEnv(rt::Tuple{VUMPSRuntime, VUMPSRuntime}, M::StructArray, alg, Fo
     ALd, ARd, Cd = map(x->atype_device!(atype, x, 1), [ALd, ARd, Cd]) # transfer device 2 data to 1
     ACd = ALCtoAC(ALd, Cd)
 
-    _, FLo =  leftenv(ALu, conj(ALd), M, Fo[1]; ifobs = true, alg)
-    _, FRo = rightenv(ARu, conj(ARd), M, Fo[2]; ifobs = true, alg)
+    _, FLo =  leftenv(ALu, ALd, M, Fo[1]; ifobs = true, alg)
+    _, FRo = rightenv(ARu, ARd, M, Fo[2]; ifobs = true, alg)
     return VUMPSEnv(ACu, ARu, ACd, ARd, FLu, FRu, FLo, FRo)
 end
 
