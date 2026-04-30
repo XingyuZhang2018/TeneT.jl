012e6cd6927057259442d9ac88d05fe9d99a7675
XingyuZhang2018 <864129714@qq.com>
2026-04-30 14:59:45 +0200

    feat(kagome): add :onehole_real example file

---
+++ examples/Heisenberg/Heisenberg_Kagome_onehole_real_VUMPS_General.jl
@@ -0,0 +1,67 @@
+using TeneT
+using Random
+using CUDA
+using OptimKit
+using LinearAlgebra
+using Zygote
+
+seed = 42
+Random.seed!(seed)
+atype = Array
+etype = Float64
+D, χ, χshift, maxiter_restart = 2, 16, 0, 10
+pattern = [1 3;
+           2 4]
+model = Heisenberg(lattice=Kagome(:onehole_real),
+                   S=0.5, Jx=1.0, Jy=1.0, Jz=1.0,
+                   ifrotate=false,
+                   couplingtype=:uniform, bondratio=1.0)
+No = 0
+SUτ = 0.0
+folder = joinpath(pkgdir(TeneT), "data/$model/$pattern/VUMPS_General/$etype/seed$seed/")
+boundary_alg = VUMPS{General}(ifupdown=true,
+                               ifdownfromup=false,
+                               ifsimple_eig=true,
+                               ifparallelupdown=false,
+                               ifparallel=false,
+                               forloop_iter=1,
+                               maxiter=30,
+                               miniter=0,
+                               maxiter_ad=4,
+                               miniter_ad=4,
+                               power_iter=5,
+                               power_iter_ad=5,
+                               power_iter_obs=40,
+                               show_every=10,
+                               tol=1e-10,
+                               verbosity=3,
+                              #  inner_etype=Float32,
+                              #  inner_etype_final_steps=2
+)
+params = GradientOptimize(model=model,
+                          pattern=pattern,
+                          boundary_alg=boundary_alg,
+                          optimizer=LBFGS(200; maxiter=50, verbosity=4, gradtol=1e-7, linesearch=HagerZhangLineSearch(maxfg=5)),
+                          forloop_iter=1,
+                          maxiter_restart=maxiter_restart,
+                          verbosity=4,
+                          folder=folder,
+                          ifSU=false,
+                          SUτ=SUτ,
+                          ifprecondition=true,
+                          iter_precond=0,
+                          reuse_env=true,
+                          ifsave_env=false,
+                          ifload_env=false,
+                          ifsave_lbfgs=true,
+                          ifload_lbfgs=false
+)
+A = init_ipeps(;atype, etype, No, D, χ, params)
+
+function restriction_ipeps(A)
+   A /= norm(A)
+   return A
+end
+
+optimise_ipeps(A, χ, χshift, params; restriction_ipeps);
+# observable(A, χ, params; restriction_ipeps)
